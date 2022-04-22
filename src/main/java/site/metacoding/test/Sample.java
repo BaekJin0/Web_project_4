abad988f2d1460a377f247cd5325cacb1746143a
jinyoung <bjy121412@gmail.com>
2022-04-22 18:01:59 +0900

    [백진영] 지도 기능

--- src/main/java/site/metacoding/test/Sample.java
+++ src/main/java/site/metacoding/test/Sample.java
@@ -1,5 +1,14 @@
 package site.metacoding.test;
 
-public class Sample {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Sample {
+    private double x;
+    private double y;
+    private String name;
 }
