a1fd047903583e972aba7871c03de7f830be2dbd
woals990605 <woals990605@naver.com>
2022-04-23 20:48:43 +0900

    [황재민] 글상세페이지, 글목록 및 페이징

--- src/main/java/site/metacoding/service/PostService.java
+++ src/main/java/site/metacoding/service/PostService.java
@@ -1,7 +1,12 @@
 package site.metacoding.service;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -14,11 +19,25 @@ public class PostService {
 
     private final RestaurantRepository restaurantRepository;
 
-    public List<Restaurant> mSearch(String keyword) {
-        if (keyword == null) {
-            return restaurantRepository.mSearch("");
+    public Page<Restaurant> mList(String keyword, Integer page) {
+        PageRequest pr = PageRequest.of(page, 20, Sort.by(Direction.DESC, "id"));
+        if (keyword.equals("")) {
+            return restaurantRepository.findAll(pr);
         } else {
-            return restaurantRepository.mSearch(keyword);
+            return restaurantRepository.mList(keyword, pr);
         }
+
+    }
+
+    public Restaurant 글상세보기(Integer id) {
+        Optional<Restaurant> postOp = restaurantRepository.findById(id);
+
+        if (postOp.isPresent()) {
+            Restaurant postEntity = postOp.get();
+            return postEntity;
+        } else {
+            throw new RuntimeException("해당 게시글을 찾을 수 없습니다.");
+        }
+
     }
 }
