ac6b8920099ea6ddfaf2685faccf3d7f0e9fdb15
woals990605 <woals990605@naver.com>
2022-04-26 19:02:14 +0900

    [황재민] 담기기능중

---
+++ src/main/java/site/metacoding/web/LoveController.java
@@ -0,0 +1,41 @@
+package site.metacoding.web;
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import lombok.RequiredArgsConstructor;
+import site.metacoding.domain.love.Love;
+import site.metacoding.domain.user.User;
+import site.metacoding.service.LoveService;
+import site.metacoding.web.dto.love.LoveReqDto;
+
+@RequiredArgsConstructor
+@Controller
+public class LoveController {
+    private final LoveService loveService;
+    private final HttpSession session;
+
+    @PostMapping("/s/post/{postId}/love")
+    public String love(@PathVariable Integer postId, LoveReqDto loveReqDto) {
+
+        User principal = (User) session.getAttribute("principal");
+
+        loveService.담기(loveReqDto, postId, principal);
+        return "redirect:/post/" + postId;
+    }
+
+    @GetMapping("/s/user/{id}/love")
+    public String myLoveList(@PathVariable Integer id, Model model) {
+        List<Love> loves = loveService.담기목록보기(id);
+        model.addAttribute("loves", loves);
+
+        return "/user/userInfo";
+    }
+}
